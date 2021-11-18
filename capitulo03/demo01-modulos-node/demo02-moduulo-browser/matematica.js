bc7215e969bf2f0aabcf87533dbddceede791877
Eduardo Neves <ematheusneves13@gmail.com>
2021-11-18 12:40:20 -0300

    Foi importado todos os modulos para funcionarem no navegadores, a calculadora já está funcionando, porém ainda é preciso criar as duas operações básicas soma e adição e trabalhar um pouco no css para stylizar a pagina

---
+++ capitulo03/demo01-modulos-node/demo02-moduulo-browser/matematica.js
@@ -0,0 +1,10 @@
+class Matematica {
+    static dividir(valor1, valor2) {
+        return valor1 / valor2
+    }
+    static subtrair(valor1, valor2) {
+        return valor1 - valor2
+    }
+}
+
+// Este arquivo fica púbico sozinho, não precisa usar o module exports nativo dp node JS.
