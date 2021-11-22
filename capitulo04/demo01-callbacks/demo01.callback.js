ccba14e69c70fd0d20a149e4d232505528144afa
Eduardo Neves <ematheusneves13@gmail.com>
2021-11-22 12:26:25 -0300

    Finalizado exercício de demonstração de callback usando o modulo fySistem, readFile e WriteFile no final sempre

---
+++ capitulo04/demo01-callbacks/demo01.callback.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+ /* fs.readFile('./arq1.txt', (error, resposta)  => {
+     if(error) {
+         console.error('Deu ruim***!!', error.stack)
+         return;
+     }
+     console.log('resposta', resposta.toString())     
+ })  */
+
+fs.readFile('./arq1.txt', (errorArq1, respostaArq1) => {
+    if(errorArq1) {
+        console.error('Deu ruim no arq1, errorArq1')
+        return;
+    }
+    fs.readFile('./arq2.txt', (errorArq2, respostaArq2) => {
+        if(errorArq2) {
+            console.error('Deu ruim no arq2, errorArq2')
+            return;
+        }
+        fs.readFile('./arq3.txt', (errorArq3, respostaArq3) => {
+            if(errorArq3) {
+                console.error('Deu ruim no Arq3', errorArq3)
+                return;
+            }
+            const conteudo = `${respostaArq1}/n${respostaArq2}/n${respostaArq3}`
+            
+            fs.writeFile('./final.txt', conteudo, (erroWrite, respostaWrite) => {
+                if(erroWrite) {
+                    console.error('Deu ruim na gravaçao', errorWrite)
+                    return;
+                }
+                console.log('Arquivo salvo com sucesso')
+            })
+        })
+    })
+})
+
+
