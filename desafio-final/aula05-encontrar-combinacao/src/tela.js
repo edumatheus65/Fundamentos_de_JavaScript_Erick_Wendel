5de4effd4ca4254b3098bbf04f95adbf3e84da31
Eduardo Neves <ematheusneves13@gmail.com>
2021-11-29 16:31:13 -0300

    Criado p diretorio aula05-encontrar-combinacao de carta

---
+++ desafio-final/aula05-encontrar-combinacao/src/tela.js
@@ -0,0 +1,34 @@
+const ID_CONTEUDO = "conteudo"
+const ID_BTN_JOGAR= "jogar"
+class Tela {
+    static obterCodigoHtml(item) {
+       return `
+        <div class="col-md-3">
+            <div class="card" style="width: 50%;" onclick="window.verificarSelecao('${item.id}', '${item.nome}')">
+                <img name="${item.nome}" src="${item.img}" class="card-img-top" alt="..." />
+            </div>
+            <br />
+        </div> 
+        `
+    }
+    static configurarBotaoVerificarSelecao(funcaoOnClick) {        
+        window.verificarSelecao = funcaoOnClick
+    }
+    static alterarConteudoHTML(codigoHtml) {
+        const conteudo = document.getElementById(ID_CONTEUDO)
+        conteudo.innerHTML = codigoHtml
+    }
+    static gerarStringHTMLPelaImagem(data) {
+        return data.map(Tela.obterCodigoHtml).join('')
+    }
+
+    static atualizarImagens(itens) {
+        const codigoHtml = Tela.gerarStringHTMLPelaImagem(itens)
+        Tela.alterarConteudoHTML(codigoHtml)
+    }
+    static configurarBotaoJogar(funcaoOnClick) {
+        const btnJogar = document.getElementById(ID_BTN_JOGAR)
+        btnJogar.onclick = funcaoOnClick
+    }
+}
+
